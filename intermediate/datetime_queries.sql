2a29e91b9332c2957d6a6c2de5d529abf3cc926d
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2024-09-20 20:59:14 +0530

    more datetime manipulations

--- intermediate/datetime_queries.sql
+++ intermediate/datetime_queries.sql
@@ -4,5 +4,24 @@ show tables;
 describe AppleSales;
 
 -- how long the apples have been in the refrigerator room
-select (timestamp(DeliveryDate) - StartingFreeze) as FreezeTime
+-- TODO: This is probably wrong. Need to be fixed!
+select StartingFreeze, DeliveryDate,
+	(extract( day from timestamp(DeliveryDate)) - extract( day from StartingFreeze)) as FreezeTimeDays
 from AppleSales;
+
+-- extract the month and day from the starting_freeze time for the year 2018
+select date(StartingFreeze) as StartingFreeze,
+	extract(month from StartingFreeze) as Month,
+	extract(day from StartingFreeze) as Day
+from AppleSales
+where year(StartingFreeze) = 2020;
+
+-- Date string formatting
+select date_format(StartingFreeze, "%Y-%m-%d") as StartingFreeze
+from AppleSales;
+
+select * from AppleSales;
+
+
+
+
